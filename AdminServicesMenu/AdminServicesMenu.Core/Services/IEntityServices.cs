d01bfbb8bb57d482313f21026145a4de7412824e
Geratoptus <shura.bocharov.2004@mail.ru>
2025-04-14 12:09:49 +0500

    Сервисы

---
+++ AdminServicesMenu/AdminServicesMenu.Core/Services/IEntityServices.cs
@@ -0,0 +1,9 @@
+namespace AdminServicesMenu.Core.Services;
+
+public interface IEntityServices
+{
+    public void Create();
+    public void Delete();
+    public void Update();
+    public void List();
+}
