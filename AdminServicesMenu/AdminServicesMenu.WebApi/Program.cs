6cde8750040ca39db9049244d1e161517ba9868d
kexogg <kexogg@gmail.com>
2025-04-14 11:52:22 +0500

    add swagger

--- AdminServicesMenu/AdminServicesMenu.WebApi/Program.cs
+++ AdminServicesMenu/AdminServicesMenu.WebApi/Program.cs
@@ -1,6 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddServer(new OpenApiServer
+        {
+            Url = "/",
+            Description = "AdminServicesMenu"
+        }
+    );
+    option.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "AdminServicesMenu",
+        Version = "v1"
+    });
+    option.DescribeAllParametersInCamelCase();
+    option.SupportNonNullableReferenceTypes();
+});
+
+builder.Services.AddControllers();
+
+builder.Services.Configure<RouteOptions>(options => { options.LowercaseUrls = true; });
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
-app.Run();
+await app.RunAsync();
